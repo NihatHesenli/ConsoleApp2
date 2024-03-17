b3efe54c4def14f50785d88a2a76485dbec24d71
Standard Computer`s <Standard Computer`s@DESKTOP-PGF9R5P>
2024-03-17 12:52:30 -0700

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        Console.WriteLine(calculator(a, b));
+        Console.WriteLine(calculato(b, c));
+
+
+    }
+    public static int calculator(int num1, int num2)
+    {
+        return num1 + num2;
+
+
+
+    }
+    public static int calculato(int num2, int num3)
+    {
+
+        return num2 * num3;
+    }
+}
+
+
+
+
+
+
+
